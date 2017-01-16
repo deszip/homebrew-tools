37963f0fa9332d726379c689947ff6ff230c647c
Deszip <raf.rafiki@gmail.com>
2017-01-16 19:24:25 +0200

    url updated to releases

--- xclean.rb
+++ xclean.rb
@@ -1,7 +1,7 @@
 class Xclean < Formula
   desc "CLI tool to clean after XCode"
   homepage "https://github.com/deszip/xclean"
-  url "https://github.com/deszip/xclean/blob/master/xclean-0.0.1.tar.gz"
+  url "https://github.com/deszip/xclean/releases/download/0.0.1/xclean-0.0.1.tar.gz"
   sha256 "d1db4b62c2f04eae7eaa4bedd0fecff5f5d9e957706fec5842e00890ab435800"
 
   bottle :unneeded
